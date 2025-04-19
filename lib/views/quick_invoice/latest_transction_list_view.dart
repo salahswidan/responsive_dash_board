1ec852c429ef89f8515d3cb382c5fa137e133d8f
salah <salahswidan212@gmail.com>
2025-04-19 21:30:04 +0200

    replace listview with used row with map

--- lib/views/quick_invoice/latest_transction_list_view.dart
+++ lib/views/quick_invoice/latest_transction_list_view.dart
@@ -23,16 +23,15 @@ class LatestTransctionListView extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: ListView.builder(
-        itemCount: items.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return IntrinsicWidth(
-              child: UserInfoListTile(userInfoModel: items[index]));
-        },
-      ),
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+          children: items
+              .map((e) => IntrinsicWidth(
+                    child: UserInfoListTile(userInfoModel: e),
+                  ))
+              .toList()),
     );
+    
   }
 }
